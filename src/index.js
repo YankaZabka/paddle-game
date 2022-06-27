22536d1a5980af81bdfcda01dccb665d1834b881
YankaZabka <yanka.zanka@gmail.com>
2022-06-27 21:23:59 +0300

    feat: add ball img

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import Paddle from './paddle'
 import InputHandler  from "./input";
 import "./style.css"
+import BallIcon from "../assets/ball.png"
 
 const GAME_WIDTH = 800
 const GAME_HEIGHT = 600
+
+const BALL_WIDTH = 40
+const BALL_HEIGHT = 40
 
 const canvas = document.createElement("canvas")
 canvas.setAttribute(`id`,`gameScreen`);
 canvas.setAttribute(`width`,`${GAME_WIDTH}`);
 canvas.setAttribute(`height`,`${GAME_HEIGHT}`);
 document.body.appendChild(canvas)
+
+const ballImg = document.createElement("img")
+ballImg.setAttribute("id", "img_ball")
+ballImg.setAttribute("src", BallIcon)
+ballImg.setAttribute(`width`,`${BALL_WIDTH}`);
+ballImg.setAttribute(`height`,`${BALL_HEIGHT}`);
+document.body.appendChild(ballImg)
 
 const ctx = canvas.getContext("2d")
 
