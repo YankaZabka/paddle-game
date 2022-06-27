d664e85d2483dbc4be966858ce5172f3e8537d13
YankaZabka <yanka.zanka@gmail.com>
2022-06-27 10:41:01 +0300

    feat: update paddle class with movement methods

--- src/paddle.js
+++ src/paddle.js
@@ -3,10 +3,27 @@ export default class Paddle {
         this.width = 150
         this.height = 25
 
+        this.maxSpeed = 6
+        this.speed = 0
+
+        this.gameWidth = gameWidth
+
         this.position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10
         }
+    }
+
+    moveLeft() {
+        this.speed = -this.maxSpeed
+    }f
+
+    moveRight() {
+        this.speed = this.maxSpeed
+    }
+
+    stop() {
+        this.speed = 0
     }
 
     draw(ctx) {
@@ -14,4 +31,12 @@ export default class Paddle {
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
+    update(deltaTime) {
+        if (!deltaTime) return
+
+        if (this.position.x < 0) this.position.x = 0
+        if (this.position.x + this.width > this.gameWidth) this.position.x = this.gameWidth - this.width
+
+        this.position.x += this.speed
+    }
 }
