852e6142a2b2fffba554662891af7010ebc7f96c
YankaZabka <yanka.zanka@gmail.com>
2022-06-27 10:41:12 +0300

    feat: add game-loop function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Paddle from './paddle'
+import InputHandler  from "./input";
 import "./style.css"
 
 const GAME_WIDTH = 800
@@ -12,11 +13,24 @@ document.body.appendChild(canvas)
 
 const ctx = canvas.getContext("2d")
 
-
-
-
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT)
-
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT)
 
+new InputHandler(paddle)
+
 paddle.draw(ctx)
+
+let lastTime = 0
+
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime
+    lastTime = timestamp
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT)
+    paddle.update(deltaTime)
+    paddle.draw(ctx)
+
+    requestAnimationFrame(gameLoop)
+}
+
+gameLoop()
